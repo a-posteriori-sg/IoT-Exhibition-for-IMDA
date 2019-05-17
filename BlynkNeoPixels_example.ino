2a31da1afa581b1a4bfbd6de9d7df0fc3aec62c7
Yoni <yoni@aposteriori.com.sg>
2019-05-18 07:45:13 +0800

    Update BlynkNeoPixels_example.ino

--- BlynkNeoPixels_example.ino
+++ BlynkNeoPixels_example.ino
@@ -1,26 +1,29 @@
 /*************************************************************
- * Created by A Posteriori LLP 
- * For Sample IoT RGB Lamp Project
- * From Blynk tutorial sample code
- * 
- * If you haven't done an IoT project, before, first Setup Blynk:
- * https://blynk.io/en/getting-started
- * 
- * If you don't know much about NeoPixels, get a set wired up to ESP8266, for instance:
- * https://www.instructables.com/id/ESP8266-controlling-Neopixel-LEDs-using-Arduino-ID/
- * 
- * Finally, integrate however you wish.
- * This Arduino code works with a Blynk module (too cheap to share and lose my B$1000 creds at this time) that has:
- * - ZeRGBa wired to V0
- * - On/Off button to V1
- * - Chase Effect On/Off button to V2
- * - Brightness Vertical Slider  to V3
- * 
- * Blynk library is licensed under MIT license
- * This example code is in public domain.
- * 
- */
-  
+  Download latest Blynk library here:
+    https://github.com/blynkkk/blynk-library/releases/latest
+
+  Blynk is a platform with iOS and Android apps to control
+  Arduino, Raspberry Pi and the likes over the Internet.
+  You can easily build graphic interfaces for all your
+  projects by simply dragging and dropping widgets.
+
+    Downloads, docs, tutorials: http://www.blynk.cc
+    Sketch generator:           http://examples.blynk.cc
+    Blynk community:            http://community.blynk.cc
+    Follow us:                  http://www.fb.com/blynkapp
+                                http://twitter.com/blynk_app
+
+  Blynk library is licensed under MIT license
+  This example code is in public domain.
+
+ *************************************************************
+
+  You can send/receive any data using WidgetTerminal object.
+
+  App project setup:
+    Terminal widget attached to Virtual Pin V1
+ *************************************************************/
+
 /* Comment this out to disable prints and save space */
 #define BLYNK_PRINT Serial
 
@@ -28,15 +31,16 @@
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 #include <Adafruit_NeoPixel.h>
+#include <string>
 
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
-char auth[] = "INSERT YOUR DEVICE AUTH TOKEN FROM BLYNK HERE";
+char auth[] = "TOKEN HERE";
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
-char ssid[] = "INSERT YOUR SSID HERE";
-char pass[] = "INSERT YOUR PASWD HERE";
+char ssid[] = "SSID HERE";
+char pass[] = "PASSWD HERE";
 
 // On a Trinket or Gemma we suggest changing this to 1:
 #define LED_PIN  D8
@@ -51,6 +55,78 @@ int onoff = 0;
 int chase = 0;
 uint32_t color = strip.Color(255, 255, 255);
 uint32_t wait = 200;
+
+ 
+void colorWipe() {
+  for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
+    strip.setPixelColor(i, color);         //  Set pixel's color (in RAM)
+    strip.show();                          //  Update strip to match
+    delay(wait);                           //  Pause for a moment
+  }
+}
+
+// Theater-marquee-style chasing lights. Pass in a color (32-bit value,
+// a la strip.Color(r,g,b) as mentioned above), and a delay time (in ms)
+// between frames.
+void theaterChase() {
+  for(int a=0; a<10; a++) {  // Repeat 10 times...
+    for(int b=0; b<3; b++) { //  'b' counts from 0 to 2...
+      strip.clear();         //   Set all pixels in RAM to 0 (off)
+      // 'c' counts up from 'b' to end of strip in steps of 3...
+      for(int c=b; c<strip.numPixels(); c += 3) {
+        strip.setPixelColor(c, color); // Set pixel 'c' to value 'color'
+      }
+      strip.show(); // Update strip with new contents
+      delay(wait);  // Pause for a moment
+    }
+  }
+}
+
+// Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
+void rainbow() {
+  // Hue of first pixel runs 5 complete loops through the color wheel.
+  // Color wheel has a range of 65536 but it's OK if we roll over, so
+  // just count from 0 to 5*65536. Adding 256 to firstPixelHue each time
+  // means we'll make 5*65536/256 = 1280 passes through this outer loop:
+  for(long firstPixelHue = 0; firstPixelHue < 5*65536; firstPixelHue += 256) {
+    for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
+      // Offset pixel hue by an amount to make one full revolution of the
+      // color wheel (range of 65536) along the length of the strip
+      // (strip.numPixels() steps):
+      int pixelHue = firstPixelHue + (i * 65536L / strip.numPixels());
+      // strip.ColorHSV() can take 1 or 3 arguments: a hue (0 to 65535) or
+      // optionally add saturation and value (brightness) (each 0 to 255).
+      // Here we're using just the single-argument hue variant. The result
+      // is passed through strip.gamma32() to provide 'truer' colors
+      // before assigning to each pixel:
+      strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(pixelHue)));
+    }
+    strip.show(); // Update strip with new contents
+    delay(wait);  // Pause for a moment
+  }
+}
+
+// Rainbow-enhanced theater marquee. Pass delay time (in ms) between frames.
+void theaterChaseRainbow() {
+  int firstPixelHue = 0;     // First pixel starts at red (hue 0)
+  for(int a=0; a<5; a++) {  // Repeat 30 times...
+    for(int b=0; b<3; b++) { //  'b' counts from 0 to 2...
+      strip.clear();         //   Set all pixels in RAM to 0 (off)
+      // 'c' counts up from 'b' to end of strip in increments of 3...
+      for(int c=b; c<strip.numPixels(); c += 3) {
+        // hue of pixel 'c' is offset by an amount to make one full
+        // revolution of the color wheel (range 65536) along the length
+        // of the strip (strip.numPixels() steps):
+        int      hue   = firstPixelHue + c * 65536L / strip.numPixels();
+        uint32_t color = strip.gamma32(strip.ColorHSV(hue)); // hue -> RGB
+        strip.setPixelColor(c, color); // Set pixel 'c' to value 'color'
+      }
+      strip.show();                // Update strip with new contents
+      delay(wait);                 // Pause for a moment
+      firstPixelHue += 65536 / 90; // One cycle of color wheel over 90 frames
+    }
+  }
+}
 
 void chaseoff() {
   chase = 0;
@@ -123,10 +199,14 @@ BLYNK_WRITE(V2)
 
 BLYNK_WRITE(V3)
 {
-  strip.setBrightness(param.asFloat()/10 * 250);
+  strip.setBrightness(param.asFloat()/10 * 250 + 1);
   strip.show();
 }
 
+BLYNK_WRITE(V4)
+{
+  theaterChaseRainbow();
+}
 
 void setup()
 {
